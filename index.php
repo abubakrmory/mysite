1a7b8151db38ace7d8647c1d9117a16d1928a84a
abubakrmory <abubakrmory@gmail.com>
2021-02-06 23:49:30 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>women's job center online</title>
+  <link rel="stylesheet" href="style.css" />
+</head>
+
+<body>
+  <section class="intro">
+    <div class="inner">
+      <div class="content">
+        <h1>women's job center online</h1>
+        <a href="#" class="btn">Get Started</a>
+      </div>
+    </div>
+  </section>
+  <p>
+    The idea of the project is an electronic platform that allows visitors to register and benefit from its services in
+    the field of job offers for women only.
+    The platform provides a page for registering companies that want to acquire employees. The platform also provides a
+    registration page for female users looking for jobs.
+    The platform makes it easy for companies offering jobs to communicate with jobseekers.
+    It is designed in a simple way, which helps users to deal with it simply, as the privacy and confidentiality of the
+    data entered into the platform are taken into consideration.
+
+  </p>
+</body>
+
+</html>
